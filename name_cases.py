d5d809bdb71b01b42dd06ff604e40d1ee9e9c49e
Ka-Ri Walker <dywayne.walker@gmail.com>
2017-07-19 21:32:53 -0500

    Added New files to project chaper 2

---
+++ name_cases.py
@@ -0,0 +1,11 @@
+person_name = "Frances"
+print("Hello " + person_name + " " + "would you like to learn some Python today")
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
+famous_quote = '"The future is for those who prepare today for it."'
+famous_person = " Malcolm X "
+print(famous_person + " " + "once said," + "\n" + famous_quote)
+print(famous_person.rstrip())
+print(famous_person.lstrip())
+print(famous_person.strip())
---
+++ simple_message.py
@@ -0,0 +1,4 @@
+message = "This is a simple message for chapter 2"
+message = "This is a message for 2-2"
+print(message)
+
