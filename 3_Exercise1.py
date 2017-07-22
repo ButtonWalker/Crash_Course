bd6a499363c91a744e6f2dd1c7ab065adeba5480
Ka-Ri Walker <dywayne.walker@gmail.com>
2017-07-21 22:42:35 -0500

    Upload the first task for chapter 3 Bicycle and exercise 1

---
+++ 3_Exercise1.py
@@ -0,0 +1,27 @@
+#create a list of my friends
+friends = ['Ronnie', 'Jason', 'Joe', 'David']
+#print out names of friends by index location
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+
+#message to Ronnie
+message = "Whats up" + " " + friends[0] + " " + "how is the damn weather ?"
+print(message)
+# message to Jason
+message = "Whats up" + " " + friends[1] + " " + "how is the damn weather ?"
+print(message)
+#message to Joe
+message = "Whats up" + " " + friends[2] + " " + "how is the damn weather ?"
+print(message)
+#message to David
+message = "Whats up" + " " + friends[-1] + " " + "how is the damn weather ?"
+print(message)
+
+#list of my favorite cars
+myCars = ["GTR", "911", "Rs3"]
+# message stating how bad i wish i had one of these
+carmessage = "I would love to have a Nissan" + " " + myCars[0] + " " + "Today!!!"
+
+print(carmessage)
---
+++ 3_bicycles.py
@@ -0,0 +1,3 @@
+bicycles = ['trek', 'cannodale', 'redline', 'specialized']
+message = "My First bicycle was a " + bicycles[0].title() + "."
+print(message)
