d1b5a47e003fc8493a6db3b2651a49ebc74b2d17
Ka-Ri Walker <dywayne.walker@gmail.com>
2017-07-21 21:42:28 -0500

    Upload Birthday and comment files from chapter 2

---
+++ birthday.py
@@ -0,0 +1,12 @@
+age = 23
+message = "Happy " + str(age) + "rd Birthday"
+print(message)
+
+print(2 + 6)
+print(8 * 1)
+print (16 / 2)
+print (10 - 2)
+
+favnumber = 14
+mymessage = "My Favorite Number is " + str(favnumber)
+print(mymessage)
---
+++ comment.py
@@ -0,0 +1,12 @@
+# Say Hello to everyone
+print("Hello Python people!")
+
+# Setting my favorite number as a variable
+myNumber = 14
+
+# Create a variable string with a message and using myNumber
+myMessage = "The number" + " " + str(myNumber) + " " + "is my favorite !"
+
+# Display my message
+print(myMessage)
+
