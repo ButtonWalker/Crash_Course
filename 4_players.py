1108d051e7a53f1dc5d98b9f62dad49595c7d0ab
Ka-Ri Walker <dywayne.walker@gmail.com>
2017-07-29 20:45:01 -0500

    excersise 3 on chapter 4

---
+++ 4_players.py
@@ -0,0 +1,4 @@
+players = ['charles', 'martha', 'michael', 'flo', 'eli']
+print("Here are the first three player of my team")
+for player in players[:3]:
+    print(player.title())
---
+++ food.py
@@ -0,0 +1,12 @@
+my_foods = ['ribs', 'pulled pork', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('Coco Puffs')
+friend_foods.append('chocolate ice cream')
+
+print('My favorite foods are:')
+print(my_foods)
+
+
+print('\nMy friend favotie foods are:')
+print(friend_foods)
